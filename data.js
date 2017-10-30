8b4fc88f4d4bf0ec52a147f4b4f0a9a4e2d43bf7
wangyess <wangyess@github.com>
2017-10-30 19:24:36 +0800

    最后一次的练习版

--- data.js
+++ data.js
@@ -1,159 +1,132 @@
-// var data=[
-//     {
-//         id: 1,
-//         name: '花花市',
-//         parent_id: 0
-//     },
-//     {
-//         id: 2,
-//         name: '背背市',
-//         parent_id: 0
-//     },
-//     {
-//         id: 3,
-//         name: '花花县',
-//         parent_id: 1,
-//     },
-//     {
-//         id: 4,
-//         name: '花一县',
-//         parent_id: 1,
-//     },
-//     {
-//         id: 5,
-//         name: '花二县',
-//         parent_id: 1,
-//     },
-//     {
-//         id: 6,
-//         name: '背背县',
-//         parent_id: 2,
-//     },
-//     {
-//         id: 7,
-//         name: '背一县',
-//         parent_id: 2,
-//     },
-//     {
-//         id: 8,
-//         name: '背二县',
-//         parent_id: 2,
-//     },
-//     {
-//         id: 9,
-//         name: '花村',
-//         parent_id: 3,
-//     },
-//     {
-//         id: 10,
-//         name: '发村',
-//         parent_id: 3,
-//     },
-//     {
-//         id: 11,
-//         name: '哈村',
-//         parent_id: 3,
-//     },
-//     {
-//         id: 12,
-//         name: '背背村',
-//         parent_id: 6,
-//     },
-//     {
-//         id: 13,
-//         name: '佩佩村',
-//         parent_id: 6,
-//     },
-//     {
-//         id: 14,
-//         name: '嘿嘿村',
-//         parent_id: 6,
-//     },
-// ];
-// //  选中页面的元素
-// var wrapper = document.querySelector('#wrapper');
-// render();
-// function render(parent_id, children_l) {
-//     //第一遍执行  没有parent_id  第二遍就有了  如果没有就返回
-//
-//     if(children_l){
-//           children_l.innerHTML='';
-//     }
-//
-//     if(parent_id===''){
-//         return;
-//     }
-//     // 如果没传parrent_id  设置 parent_id=0  为父集
-//     //parent_id = parseInt(parent_id) || 0;
-//     if(parent_id){
-//         parent_id=parseInt(parent_id);
-//     }else{
-//         parent_id=0;
-//     }
-//
-//     //找到 为parent_id  为父集的元素
-//     // 第一遍 为0  所以parent_id=0  为腹肌
-//
-//      var list= data.filter(function (item) {
-//         // if(parent_id===item.parent_id){
-//         //     return true;
-//         // }
-//          return parent_id === item.parent_id;
-//     });
-//      if(!list.length){
-//          return;
-//      }
-//     var el=document.createElement('span');
-//     el.innerHTML=`
-//         <select> </select>
-//
-//         <span class="children"> </span>
-//     `;
-//     //  选中 下拉菜单 和 span 标签
-//
-//     var select=el.querySelector('select');
-//     var children=el.querySelector('.children');
-//
-//     //设置默认的东西
-//     var a_option=document.createElement('option');
-//     a_option.innerText='-请选择-';
-//     a_option.value='';
-//      select.appendChild(a_option);
-//
-//      //把拿出来的腹肌  发在里面
-//
-//     list.forEach(function (item) {
-//         var option=document.createElement('option');
-//         option.innerHTML=item.name;
-//         option.value=item.id;
-//         select.appendChild(option);
-//     });
-//
-//     if(!parent_id){
-//         document.querySelector('#wrapper').appendChild(el);
-//     }else{
-//         if(children_l){
-//             children_l.appendChild(el);
-//         }
-//
-//     }
-//
-//     select.addEventListener('change',function () {
-//         render(this.value ,children)
-//     })
-//
-//
-//
-//
-// }
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+var data=[
+    {
+        name:'吉林',
+        id:1,
+        parent_id:0,
+    },
+    {
+        name:'辽宁',
+        id:2,
+        parent_id:0,
+    },
+    {
+        name:'黑龙江',
+        id:3,
+        parent_id:0,
+    },
+    {
+        name:'长春' ,
+        id:4,
+        parent_id:1,
+    },
+    {
+        name:'四平' ,
+        id:5,
+        parent_id:1,
+    },
+    {
+        name:'白城' ,
+        id:6,
+        parent_id:1,
+    },
+    {
+        name:'昌图' ,
+        id:7,
+        parent_id:2,
+    },
+    {
+        name:'大连' ,
+        id:8,
+        parent_id:2,
+    },
+    {
+        name:'瓦房店' ,
+        id:9,
+        parent_id:2,
+    },
+    {
+        name:'齐齐哈尔' ,
+        id:10,
+        parent_id:3,
+    },
+    {
+        name:'哈尔滨' ,
+        id:11,
+        parent_id:3,
+    },
+    {
+        name:'龙江' ,
+        id:12,
+        parent_id:3,
+    },
+];
+
+//首先选中页面的p标签我们 要把我们做好的加到里面去
+var wrapper=document.getElementById('wrapper');
+render();
+function render(parent_id,children_1) {
+
+    //判断有没有children_1 有的 清空的值 让他初始化
+    if(children_1){
+        children_1.innerHTML='';
+    }
+    //判断有没有parent_id 如果没有继续执行 有的话判断的是不是空字符串
+    if(parent_id===''){
+        return;
+    }
+    // 我们要先找出第一个级 渲染出来  因为都有parent_id  所以我们还要先定义一个腹肌
+    //定义腹肌
+    //因为第一次遍历没有 parent_id  所以腹肌就是为0 的对象
+    parent_id=parseInt(parent_id)  || 0;
+    //找出所有腹肌元素  用filter  因为他返回的是 所有符合条件的对象 组成的数组 用别的要多些一些代码 但是也可以实现
+    var list=data.filter(function (item) {
+        return  item.parent_id===parent_id;
+    })
+    //找到所有腹肌组成的数组  还要判断一下  判断这个数组是否有长度  如果没有长度就 结束
+    if(!list.length){
+        return;
+    }
+
+    //找到腹肌之后 还要渲染到页面上 因为每次点击 都会出来一个 继续选择的
+    // 因为渲染出来继续选择哪个还是和以前的在同一行所以在定义一个span标签    这样我们一起定义两个
+    var el=document.createElement('span');
+    el.innerHTML=`
+       <select></select>
+       <span class="children"></span>
+    `;
+    // 选中我们定义好的  因为我们后面还要 把option 加到里面去 因为每个select 都有option
+    // 还有就是不要忘记给option 赋值 他们每个都有值得
+    var select=el.querySelector('select');
+    var children=el.querySelector('.children');
+
+    //这回我们开始设置默认的选项  每一个都有一个叫 请选择 的默认选项来提示我们去选择 所以我们开始设置
+    var a_option=document.createElement('option');
+    a_option.innerHTML='-请选择-';
+    a_option.value='';
+    select.appendChild(a_option);
+
+    //现在我们开始遍历 我们找到的腹肌  把他们都加进去
+    list.forEach(function (item) {
+        var option=document.createElement('option');
+        option.innerHTML=item.name;
+        option.value=item.id;
+        select.appendChild(option);
+    });
+
+    //判断是否有 传入的参数 parent_id  如果没有 代表第一次执行 我们把值赋给 页面的p标签
+    if(!parent_id){
+        wrapper.appendChild(el);
+    }else{
+        if(children_1){
+            children_1.appendChild(el);
+        }
+    }
+
+    select.addEventListener('change',function () {
+        render(this.value,children);
+    })
+}
+
+
+
+
--- main.js
+++ main.js
@@ -1,145 +1,145 @@
-var data=[
-    {
-        name:'吉林',
-        id:1,
-        parent_id:0,
-    },
-    {
-        name:'辽宁',
-        id:2,
-        parent_id:0,
-    },
-    {
-        name:'黑龙江',
-        id:3,
-        parent_id:0,
-    },
-    {
-      name:'长春' ,
-        id:4,
-        parent_id:1,
-    },
-    {
-        name:'四平' ,
-        id:5,
-        parent_id:1,
-    },
-    {
-        name:'白城' ,
-        id:6,
-        parent_id:1,
-    },
-    {
-        name:'昌图' ,
-        id:7,
-        parent_id:2,
-    },
-    {
-        name:'大连' ,
-        id:8,
-        parent_id:2,
-    },
-    {
-        name:'瓦房店' ,
-        id:9,
-        parent_id:2,
-    },
-    {
-        name:'齐齐哈尔' ,
-        id:10,
-        parent_id:3,
-    },
-    {
-        name:'哈尔滨' ,
-        id:11,
-        parent_id:3,
-    },
-    {
-        name:'龙江' ,
-        id:12,
-        parent_id:3,
-    },
-];
-
-var wrapper=document.getElementById('wrapper');
-render();
-function render(parent_id,children_1) {
-    if(children_1){
-        children_1.innerHTML='';
-    }
-    if(parent_id===''){
-        return;
-    }
-    //首先我要把他的腹肌便利出来
-    //因为他们都有parent_id 所以我要定义一个腹肌的id
-    //第一遍遍历的时候0  就是腹肌ID
-    parent_id=parseInt(parent_id) || 0;
-
-    //开始便利出以0为腹肌的对象
-    //这里为什么用 filter  一因为用他返回的是一个复合条件的对象  组成的数组  用其他也可以只不过稍微多写几行代码
-    var list =data.filter(function (item) {
-        return item.parent_id===parent_id;
-    });
-
-    if(!list.length){
-        return;
-    }
-    // 腹肌找到了  这回我们把页面上的初始值定义一下 因为刚开始出来一个下拉菜单  但是点击里面一个值得时候还想出来一个下拉菜单  所以我们要一起定义两个
-    var el=document.createElement('span');
-    el.innerHTML=`
-       <select></select>
-       <span class="children"></span>
-    `;
-
-      //这回选中上面创建的标签  为了一会向里面添加东西
-    var select=el.querySelector('select');
-    var children=el.querySelector('.children');
-
-    //定义一个 叫请选择 的option  因为每个这种 选项都会有这样的一个提示  所以要写一个
-    //首先创建一个 option  在吧里面加入  文字  请选择  最后加到select中  记住一定要赋值 value
-    var a_option=document.createElement('option');
-    a_option.innerHTML='-请选择-';
-    a_option.value='';
-    select.appendChild(a_option);
-
-    //这回我们把遍历出来的腹肌 组 放进去
-
-    list.forEach(function (item) {
-        var option=document.createElement('option');
-        option.innerHTML=item.name;
-        option.value=item.id;
-        select.appendChild(option);
-    });
-
-
-    if(!parent_id){
-        wrapper.appendChild(el);
-    }else{
-        if(children_1){
-            children_1.appendChild(el);
-        }
-    }
-
-    select.addEventListener('change',function () {
-        render(this.value,children);
-    })
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// var data=[
+//     {
+//         name:'吉林',
+//         id:1,
+//         parent_id:0,
+//     },
+//     {
+//         name:'辽宁',
+//         id:2,
+//         parent_id:0,
+//     },
+//     {
+//         name:'黑龙江',
+//         id:3,
+//         parent_id:0,
+//     },
+//     {
+//       name:'长春' ,
+//         id:4,
+//         parent_id:1,
+//     },
+//     {
+//         name:'四平' ,
+//         id:5,
+//         parent_id:1,
+//     },
+//     {
+//         name:'白城' ,
+//         id:6,
+//         parent_id:1,
+//     },
+//     {
+//         name:'昌图' ,
+//         id:7,
+//         parent_id:2,
+//     },
+//     {
+//         name:'大连' ,
+//         id:8,
+//         parent_id:2,
+//     },
+//     {
+//         name:'瓦房店' ,
+//         id:9,
+//         parent_id:2,
+//     },
+//     {
+//         name:'齐齐哈尔' ,
+//         id:10,
+//         parent_id:3,
+//     },
+//     {
+//         name:'哈尔滨' ,
+//         id:11,
+//         parent_id:3,
+//     },
+//     {
+//         name:'龙江' ,
+//         id:12,
+//         parent_id:3,
+//     },
+// ];
+//
+// var wrapper=document.getElementById('wrapper');
+// render();
+// function render(parent_id,children_1) {
+//     if(children_1){
+//         children_1.innerHTML='';
+//     }
+//     if(parent_id===''){
+//         return;
+//     }
+//     //首先我要把他的腹肌便利出来
+//     //因为他们都有parent_id 所以我要定义一个腹肌的id
+//     //第一遍遍历的时候0  就是腹肌ID
+//     parent_id=parseInt(parent_id) || 0;
+//
+//     //开始便利出以0为腹肌的对象
+//     //这里为什么用 filter  一因为用他返回的是一个复合条件的对象  组成的数组  用其他也可以只不过稍微多写几行代码
+//     var list =data.filter(function (item) {
+//         return item.parent_id===parent_id;
+//     });
+//
+//     if(!list.length){
+//         return;
+//     }
+//     // 腹肌找到了  这回我们把页面上的初始值定义一下 因为刚开始出来一个下拉菜单  但是点击里面一个值得时候还想出来一个下拉菜单  所以我们要一起定义两个
+//     var el=document.createElement('span');
+//     el.innerHTML=`
+//        <select></select>
+//        <span class="children"></span>
+//     `;
+//
+//       //这回选中上面创建的标签  为了一会向里面添加东西
+//     var select=el.querySelector('select');
+//     var children=el.querySelector('.children');
+//
+//     //定义一个 叫请选择 的option  因为每个这种 选项都会有这样的一个提示  所以要写一个
+//     //首先创建一个 option  在吧里面加入  文字  请选择  最后加到select中  记住一定要赋值 value
+//     var a_option=document.createElement('option');
+//     a_option.innerHTML='-请选择-';
+//     a_option.value='';
+//     select.appendChild(a_option);
+//
+//     //这回我们把遍历出来的腹肌 组 放进去
+//
+//     list.forEach(function (item) {
+//         var option=document.createElement('option');
+//         option.innerHTML=item.name;
+//         option.value=item.id;
+//         select.appendChild(option);
+//     });
+//
+//
+//     if(!parent_id){
+//         wrapper.appendChild(el);
+//     }else{
+//         if(children_1){
+//             children_1.appendChild(el);
+//         }
+//     }
+//
+//     select.addEventListener('change',function () {
+//         render(this.value,children);
+//     })
+// }
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
